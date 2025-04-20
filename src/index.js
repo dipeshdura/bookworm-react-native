47db5967e15e38884f9a8fba0e4caa8acc569386
dipeshdura <dipeshdura14@gmail.com>
2025-04-20 22:00:55 +0545

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express"; 
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import {connectDB} from "./lib/db.js";
+const app =express();
+
+const PORT =process.env.PORT || 3000
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth",authRoutes);
+app.use("/api/books",bookRoutes);
+
+//server starting point
+app.listen(PORT,()=>{
+    console.log(`Server is running on Port: ${PORT}`);
+    connectDB();
+})

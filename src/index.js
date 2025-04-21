29e150b0fe288359c61fb52c3d37cfdf5d2ff119
dipeshdura <dipeshdura14@gmail.com>
2025-04-21 16:08:40 +0545

    cron added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from "express"; 
 import cors from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
-import {connectDB} from "./lib/db.js";
-const app =express();
 
+import {connectDB} from "./lib/db.js";
+
+const app =express();
 const PORT =process.env.PORT || 3000
 
+job.start();
 //middleware
 app.use(express.json());
 app.use(cors());
